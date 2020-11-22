28bb990ebcb05404ae3723ab9a794bb0f02d841e
shobhit sharma <shobhitsharma.3661@gmail.com>
2020-11-22 21:37:01 +0530

    11/22/2020

--- script.js
+++ script.js
@@ -5,15 +5,35 @@ var start = document.querySelector("#st");
 var restart = document.querySelector("#re");
 var solve = document.querySelector("#so");
 var reset = document.querySelector("#b4");
+var clicked = true;
 //check button calls
 
 start.addEventListener('click',startTheGame);
-//start button
+reset.addEventListener('click',resetTable);
 
+
+//reset button reset the table
+function resetTable()
+{
+
+}
+
+//start button
 function startTheGame()
 {
-  for(var i=0;i<15;i++)
+  if(clicked==true)
   {
-      cell[i].textContent =  Math.floor((Math.random() * 9)+1);
+    for(var i=0;i<38;i++)
+    {
+        cell[Math.floor((Math.random() * cell.length))].textContent =  Math.floor((Math.random() * 9)+1);
+    }
+
+    clicked = false;
+    return;
+  }
+
+  if(clicked==false)
+  {
+    alert("Game is already started");
   }
 }
