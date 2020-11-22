aef18007b3af1d99d9a0658604b961f7be225510
shobhit sharma <shobhitsharma.3661@gmail.com>
2020-11-22 22:34:18 +0530

    11/22/2020

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var start = document.querySelector("#st");
 var restart = document.querySelector("#re");
 var solve = document.querySelector("#so");
 var reset = document.querySelector("#b4");
+var initialMatrix = ;
 var clicked = true;
 //check button calls
 
@@ -15,7 +16,10 @@ reset.addEventListener('click',resetTable);
 //reset button reset the table
 function resetTable()
 {
-
+    for(int i=0;i<cell.length;i++)
+    {
+      initialMatrix[i].textContent = cell[i].textContent;
+    }
 }
 
 //start button
@@ -26,8 +30,8 @@ function startTheGame()
     for(var i=0;i<38;i++)
     {
         cell[Math.floor((Math.random() * cell.length))].textContent =  Math.floor((Math.random() * 9)+1);
+        
     }
-
     clicked = false;
     return;
   }
